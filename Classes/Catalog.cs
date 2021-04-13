68dae934352e843960adf66bd044f60c1e632c1e
Brad Thompson <bdthompson@ualr.edu>
2021-04-13 10:39:55 -0500

    Update Catalog.cs

--- Classes/Catalog.cs
+++ Classes/Catalog.cs
@@ -7,5 +7,30 @@ namespace TLDR_Capstone.Classes
 {
 	public class Catalog
 	{
-	}
+        //Constructor
+        public Catalog(List<Course> pCourses)
+        {
+            courses = pCourses;
+        }
+
+        //Members
+        public List<Course> courses;
+
+        //Add a single course
+        public void addCourse(Course pCourse)
+        {
+            courses.Add(pCourse);
+        }
+
+        //Getters and Setters
+        public List<Course> getCourses()
+        {
+            return courses;
+        }
+
+        public void setCourses(List<Course> courses)
+        {
+            this.courses = courses;
+        }
+    }
 }
